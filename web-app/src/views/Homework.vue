4a585cc51aa36f63b3d13e5281ab2c305eed62bd
minwoo <mwoodev526@gmail.com>
2021-08-11 03:40:49 +0900

    [update] homework file

--- web-app/src/views/Homework.vue
+++ web-app/src/views/Homework.vue
@@ -1,58 +1,79 @@
 <template>
   <div>
     <div class="input-wrap">
-      <h2 class="wrap__title">Input</h2>
+      <h2 class="wrap__title">[입력]</h2>
       <div class="input-box">
         <label for="url">URL</label>
-        <input type="text" name="url" id="url" v-model="data.url">
+        <input type="text" name="url" id="url" v-model="body.url">
       </div>
       <div class="input-box">
-        <label for="type">타입</label>
-        <select name="type" id="type" v-model="data.type">
+        <label for="type">Type</label>
+        <select name="type" id="type" v-model="body.type">
           <option v-for="(type, index) in types" :key="index" :value="type.value">
             {{ type.name }}
           </option>
         </select>
       </div>
       <div class="input-box">
-        <label for="amount">양</label>
-        <input type="number" name="amount" id="amount" v-model.number="data.amount">
+        <label for="amount">출력 단위 묶음</label>
+        <input type="number" name="amount" id="amount" v-model.number="body.amount">
       </div>
-      <button @click="printHomework">출력</button>
+      <div class="btn-wrap">
+        <button @click="submitHomework">출력</button>
+      </div>
+    </div>
+    <div class="input-wrap" v-if="isResponse">
+      <h2 class="wrap__title">[출력]</h2>
+      <div class="input-box">
+        <label for="quota">몫</label>
+        <p id="quota">{{ result?.quota }}</p>
+      </div>
+      <div class="input-box">
+        <label for="remainder">나머지</label>
+        <p id="remainder">{{ result?.remainder }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { isValidUrl } from '../libs/isValidUrl'
+import { isValidUrl } from '@/libs/isValidUrl'
+import { API } from "@/apis";
 export default {
   name: 'Homework',
   data(){
     return {
-      data: {
+      body: {
         url: '',
         type: '',
         amount: 0
       },
+      result: {},
       types : [
         {
-          name: '전체',
-          value: 'total'
+          name: 'HTML 태그 제외',
+          value: 'exceptTag'
         },
         {
-          name: '테그만 제외',
-          value: 'exceptTag'
+          name: 'Text 전체',
+          value: 'allText'
         }
       ]
     }
   },
+  computed:{
+    isResponse() {
+      return Object.keys(this.result).length
+    }
+  },
   methods: {
-    printHomework(){
+    async submitHomework(){
       try{
-        this.checkUrl(this.data.url)
-        this.checkType(this.data.type)
-        this.checkAmount(this.data.amount)
-
+        this.checkUrl(this.body.url)
+        this.checkType(this.body.type)
+        this.checkAmount(this.body.amount)
+        const response = await API.postHomeWork(this.body)
+        this.result = response?.data
       } catch (e) {
         alert(e.message)
       }
@@ -63,36 +84,26 @@ export default {
       if(!isValid) throw new Error('url 형식이 유효하지 않습니다.')
     },
     checkType(type){
-      console.log(type)
       if(!type) throw new Error('type을 선택해주세요.')
       const isValid = this.types.some(data => data.value === type)
       if(!isValid)
-        throw new Error('유효하지 않은 type입니다')
+        throw new Error('유효하지 않은 type입니다.')
     },
     checkAmount(amt){
       if(amt === undefined) throw new Error('amount을 입력해주세요.')
       const isValid = typeof amt === 'number' && amt > 0
       if(!isValid)
-        throw new Error('유효하지 않은 amount입니다')
+        throw new Error('유효하지 않은 amount입니다.')
     }
 
   }
 }
 </script>
+
 <style>
-.input-wrap{
-  width: 50%;
-  margin: 1rem auto 0;
-}
-.wrap__title{
-  width: 100%;
-  font-style: normal;
-  font-weight: bold;
-  margin: 0;
-}
-.input-box {
-  padding: 5px 0;
-  border-bottom: 1px solid #ebebeb;
+p{
+  display: inline-block;
+  width: 75%;
 }
 input{
   width: 75%;
@@ -111,6 +122,31 @@ label{
   display: inline-block;
   width: calc(25% - 10px);
   padding: 10px 0 5px;
-  font-size: 0.8rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+button{
+  padding: 0.3rem 1.5rem;
+  font-size: 1rem;
+}
+
+.input-wrap{
+  width: 50%;
+  margin: 10rem auto 0;
+}
+.btn-wrap{
+  width: 100%;
+  text-align: center;
+  margin: 4rem 0 0
+}
+.wrap__title{
+  width: 100%;
+  font-style: normal;
+  font-weight: bold;
+  margin: 0 0 1.5rem;
+}
+.input-box {
+  padding: 5px 0;
+  border-bottom: 1px solid #ebebeb;
 }
 </style>
